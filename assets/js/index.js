3c59ef2c929aaa034bd2b7a2e710642cfd958c01
0ussamaBernou <oussama_bernou@um5.ac.ma>
2022-09-26 20:35:49 +0100

    added search when pressing Enter

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,21 +1,29 @@
 
-// Search Function
- 
+const searchButton = document.querySelector('.searchButton')
 let classNum
-document.querySelector('.searchButton').addEventListener("click", search)
+searchButton.addEventListener("click", search)
 
-function search(){
+// search when pressing Enter
+window.addEventListener("keypress", (event) => {
+	if (event.key == "Enter") {
+		search()
+	}
+})
+
+// Search Function
+
+function search() {
 	classNumStr = document.querySelector('.searchInput').value
 	classNum = parseInt(classNumStr)
 	let tragetURL
-	if(Number.isInteger(classNum) && classNum > 0){
-		if(classNum <= 47)
-		classNum < 10 ?  window.location.replace(`https://100devsfollowalong.netlify.app/classes/class-0${classNum}.html`) : window.location.replace(`https://100devsfollowalong.netlify.app/classes/class-${classNum}.html`)
-		else{
+	if (Number.isInteger(classNum) && classNum > 0) {
+		if (classNum <= 47)
+			classNum < 10 ? window.location.replace(`https://100devsfollowalong.netlify.app/classes/class-0${classNum}.html`) : window.location.replace(`https://100devsfollowalong.netlify.app/classes/class-${classNum}.html`)
+		else {
 			alert(`We don't have that class yet.`)
 		}
 	}
-	else{
+	else {
 		alert(`${classNumStr} is not a valid class number.`)
 	}
 }
